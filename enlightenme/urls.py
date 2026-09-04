5d77db10e20a46b653be780fcee868b622a481c4
Hassan Shereen <connectcap890@gmail.com>
2026-09-04 06:45:55 +0500

    added openapi.yaml

--- enlightenme/urls.py
+++ enlightenme/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+from django.http import FileResponse
+import os
 
+def serve_openapi(request):
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'openapi.yaml')
+    return FileResponse(open(file_path, 'rb'), content_type='application/yaml')
 urlpatterns = [
+    path('openapi.yaml', serve_openapi),
     path('api/lectures/', include('lectures.urls')),
     path('api/quizzes/',  include('quizzes.urls')),
     path('api/users/',    include('users.urls')),
