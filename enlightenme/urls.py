068938ff8c36f4c478b157c659dc7ea3d1378c15
Hassan Shereen <connectcap890@gmail.com>
2026-09-04 06:51:07 +0500

    openapi bug fixes

--- enlightenme/urls.py
+++ enlightenme/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from django.http import FileResponse
+from django.conf import settings
 import os
 
 def serve_openapi(request):
-    file_path = os.path.join(os.path.dirname(__file__), '..', 'openapi.yaml')
+    file_path = os.path.join(settings.BASE_DIR, 'openapi.yaml')
     return FileResponse(open(file_path, 'rb'), content_type='application/yaml')
+
 urlpatterns = [
-    path('openapi.yaml', serve_openapi),
+    path('openapi.yaml',  serve_openapi),
     path('api/lectures/', include('lectures.urls')),
     path('api/quizzes/',  include('quizzes.urls')),
     path('api/users/',    include('users.urls')),
