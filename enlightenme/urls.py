6c8733b5f24992460394b60692bfcd1768e9c2c1
Hassan Shereen <connectcap890@gmail.com>
2026-09-04 06:57:41 +0500

    yaml fixes

--- enlightenme/urls.py
+++ enlightenme/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from django.http import FileResponse
+from django.http import HttpResponse
 from django.conf import settings
 import os
 
 def serve_openapi(request):
     file_path = os.path.join(settings.BASE_DIR, 'openapi.yaml')
-    return FileResponse(open(file_path, 'rb'), content_type='application/yaml')
+    with open(file_path, 'r') as f:
+        content = f.read()
+    return HttpResponse(content, content_type='application/yaml')
 
 urlpatterns = [
     path('openapi.yaml',  serve_openapi),
